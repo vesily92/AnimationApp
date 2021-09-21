60de02589918977566ac5a07c7895110caf849e7
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-21 15:44:10 +0300

    Model setup

--- AnimationApp/Models/Animation.swift
+++ AnimationApp/Models/Animation.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Float
+    let duration: Float
+    let delay: Float
+}
+
+extension Animation {
+    static func getAnimation() -> Animation {
+        let presets = DataSource.shared.presets.shuffled()
+        let curves = DataSource.shared.curves.shuffled()
+        
+        let animation = Animation(
+            preset: presets.first ?? "",
+            curve: curves.first ?? "",
+            force: DataSource.shared.force,
+            duration: DataSource.shared.duration,
+            delay: DataSource.shared.delay)
+        
+        return animation
+    }
+}
