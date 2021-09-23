8551c5b654afaeffc93d1bace49353721dc22ee6
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-23 09:41:06 +0300

    Model update

--- AnimationApp/Models/Animation.swift
+++ AnimationApp/Models/Animation.swift
@@ -21,16 +21,10 @@ extension Animation {
         let animation = Animation(
             preset: DataSource.shared.presets.shuffled().first ?? "",
             curve: DataSource.shared.curves.shuffled().first ?? "",
-            force: Float.random(in: 0.5...2), //DataSource.shared.force,
-            duration: Float.random(in: 1...3), //DataSource.shared.duration,
-            delay: Float.random(in: 0.5...1) //DataSource.shared.delay
+            force: Float.random(in: 0.5...2),
+            duration: Float.random(in: 1...3),
+            delay: Float.random(in: 0.5...1)
         )
-        /*
-         Не знаю что случилось, но изначально всё работало (хотя теперь я не уверен) с сервисным слоем
-         как надо (значения каждый раз обновлялись). После обновления икскода,
-         пришлось перенести вычисления сюда. Хотя скорее всего, я просто не вижу
-         чего-то очевидного - глаза замылились)
-         */
         
         return animation
     }
